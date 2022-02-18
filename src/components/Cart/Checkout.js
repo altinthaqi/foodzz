1594ac72121de1a6334d1f725b72a8a569cb3fc9
Altin Thaçi <altin.thaqi.520@gmail.com>
2022-02-18 16:54:14 +0100

    Finished Form Validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { useRef, useState } from "react";
-
-const isEmpty = (value) => value.trim().length === "";
-const isFiveChars = (value) => value.trim().length !== 5;
+import classes from "./Checkout.module.css";
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
   const [formInputsValidity, setFormInputsValidity] = useState({
@@ -48,24 +48,38 @@ const Checkout = (props) => {
     }
   };
 
+  const nameControlClass = `${formInputsValidity.name ? "" : classes.invalid}`;
+  const streetControlClass = `${
+    formInputsValidity.street ? "" : classes.invalid
+  }`;
+  const cityControlClass = `${formInputsValidity.city ? "" : classes.invalid}`;
+  const postalCodeControlClass = `${
+    formInputsValidity.postalCode ? "" : classes.invalid
+  }`;
+
   return (
     <Form onSubmit={confirmHandler}>
-      <Control>
+      <Control className={nameControlClass}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
         {!formInputsValidity.name && <p>Enter a valid name</p>}
       </Control>
-      <Control>
+      <Control className={streetControlClass}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
+        {!formInputsValidity.street && <p>Enter a valid street</p>}
       </Control>
-      <Control>
+      <Control className={postalCodeControlClass}>
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>Enter a valid Postal Code (5char)</p>
+        )}
       </Control>
-      <Control>
+      <Control className={cityControlClass}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Enter a valid city</p>}
       </Control>
       <Actions>
         <button type="button" onClick={props.onCancel}>
@@ -77,19 +91,31 @@ const Checkout = (props) => {
   );
 };
 
+export const Control = styled.div`
+  label {
+    font-weight: bold;
+    display: block;
+  }
+
+  input {
+    font: inherit;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 20rem;
+    max-width: 100%;
+  }
+`;
+
+export const Form = styled.form`
+  margin: 0rem 0;
+  height: 12rem;
+  overflow: auto;
+`;
+
 export const Actions = styled.div`
   display: flex;
   justify-content: flex-end;
   gap: 1rem;
-
-  .invalid label {
-    color: #ca3e51;
-  }
-
-  .invalid input {
-    border-color: #aa0b20;
-    background-color: #ffeff1;
-  }
 
   button {
     font: inherit;
@@ -116,27 +142,6 @@ export const Actions = styled.div`
   .submit:active {
     background-color: #7a2706;
   }
-`;
-
-export const Control = styled.div`
-  label {
-    font-weight: bold;
-    display: block;
-  }
-
-  input {
-    font: inherit;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    width: 20rem;
-    max-width: 100%;
-  }
-`;
-
-export const Form = styled.form`
-  margin: 0rem 0;
-  height: 12rem;
-  overflow: auto;
 `;
 
 export default Checkout;
